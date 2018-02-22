3ef3081395ff50dcc94a59a4113388a05301269f
mac <mac@macdeMacBook-Pro.local>
2018-02-22 22:28:09 +0800

    父子路由嵌套

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,50 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { ShoplistComponent } from './components/shoplist/shoplist.component';
+import { ShopcateComponent } from './components/shopcate/shopcate.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'welcome',
+        component: WelcomeComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'welcome'
+      }
+    ]
+  },
+  {
+    path: 'shop',
+    component: ShopComponent,
+    children: [
+      {
+        path: 'shoplist',
+        component: ShoplistComponent
+      },
+      {
+        path: 'shopcate',
+        component: ShopcateComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'shoplist'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { ShoplistComponent } from './components/shoplist/shoplist.component';
+import { ShopcateComponent } from './components/shopcate/shopcate.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    WelcomeComponent,
+    ShoplistComponent,
+    ShopcateComponent
   ],
   imports: [
     BrowserModule,
